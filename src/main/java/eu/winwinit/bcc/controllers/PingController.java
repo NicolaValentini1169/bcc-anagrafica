0cf8d58971aa54850ac8c13ebc2299013b39175e
Antonio Fasolato <fasolato@omicronconsulting.it>
2019-09-30 16:43:48 +0200

    Ping service

---
+++ src/main/java/eu/winwinit/bcc/controllers/PingController.java
@@ -0,0 +1,15 @@
+package eu.winwinit.bcc.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    public ResponseEntity<String> sayHello() {
+        return new ResponseEntity<>("PING", HttpStatus.OK);
+    }
+}
