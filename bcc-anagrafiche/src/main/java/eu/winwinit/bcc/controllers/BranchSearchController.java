15ee5ad5345ba1e4adba112c82368ad967134e6e
sinigaglia winwinit <gianluca.sinigaglia@winwinit.eu>
2019-10-02 15:25:56 +0200

    Added the control for the authority of an user

--- bcc-anagrafiche/src/main/java/eu/winwinit/bcc/controllers/BranchSearchController.java
+++ bcc-anagrafiche/src/main/java/eu/winwinit/bcc/controllers/BranchSearchController.java
@@ -7,7 +7,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import eu.winwinit.bcc.entities.Filiali;
 import eu.winwinit.bcc.security.JwtTokenProvider;
 import eu.winwinit.bcc.security.SecurityConstants;
+import eu.winwinit.bcc.util.UtilClass;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -27,23 +27,23 @@ public class BranchSearchController {
 	 @RequestMapping(value = "/branch-search", method = RequestMethod.GET)
 	    public ResponseEntity<List<Filiali>> branchSearch(
 	    		@RequestHeader(value=SecurityConstants.HEADER_STRING) String jwtToken) {
-		 
-		 Set<GrantedAuthority> rolesSet = jwtTokenProvider.getRolesFromJWT(jwtToken);
+		 Set<String> rolesSetString = UtilClass.fromGrantedAuthorityToStringSet(jwtTokenProvider.getRolesFromJWT(jwtToken));
 		 List<Filiali> filialiList = new ArrayList<Filiali>();
-		 // TODO: Controllo sull'utente che sia admin/utente
-//		 if(rolesSet.contains(SecurityConstants.ROLE_ADMIN)) {
-//		 } else if(rolesSet.contains(SecurityConstants.ROLE_USER)) {
-//		 }
-		 
-		 Filiali filiale1 = new Filiali();
-		 filiale1.setNome("Verona");
-		 Filiali filiale2 = new Filiali();
-		 filiale2.setNome("Padova");
-		 Filiali filiale3 = new Filiali();
-		 filiale3.setNome("Milano");
-		 filialiList.add(filiale1);
-		 filialiList.add(filiale2);
-		 filialiList.add(filiale3);
+		 if(rolesSetString.contains("ROLE_" + SecurityConstants.ROLE_ADMIN)) {
+			 Filiali filiale1 = new Filiali();
+			 filiale1.setNome("Verona");
+			 Filiali filiale2 = new Filiali();
+			 filiale2.setNome("Padova");
+			 Filiali filiale3 = new Filiali();
+			 filiale3.setNome("Milano");
+			 filialiList.add(filiale1);
+			 filialiList.add(filiale2);
+			 filialiList.add(filiale3);
+		 } else if(rolesSetString.contains("ROLE_" + SecurityConstants.ROLE_USER)) {
+			 Filiali filiale1 = new Filiali();
+			 filiale1.setNome("Verona");
+			 filialiList.add(filiale1);
+		 }
 		 
 		 return new ResponseEntity<>(filialiList, HttpStatus.OK);
 	    }
