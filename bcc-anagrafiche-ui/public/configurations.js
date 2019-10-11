2093140b068068d8148b3376582bfcbfbe90ac1f
Ferrazzo Alberto <ferrazzo@omicronconsulting.it>
2019-10-11 10:06:56 +0200

    changed comment in configurations

--- bcc-anagrafiche-ui/public/configurations.js
+++ bcc-anagrafiche-ui/public/configurations.js
@@ -1,6 +1,6 @@
 //things to modify:
 //app_url and url_prefix must match .war name in tomcat web apps folder ex: url_prefix:/bcc-anagrafiche/ and bcc-anagrafiche.war
-//REACT_APP_URL_JAVA is the address to make react comunicate with backend. The name after port is the name of the .war in tomcat folder
+//REACT_APP_URL_JAVA is the address to make react comunicate with backend. The name after port is the name of the .war in tomcat folder for the backend file
 
 //for localhost:
 // app_url: http://localhost:yourPort/
